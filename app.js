a55141e0f76c0e5fb3d5f1a16c3289fa4fc9c6f6
Haseeb Anwar <haseebanwar99@gmail.com>
2020-09-12 21:34:39 +0500

    Configured app for asset compression with Brotli

--- app.js
+++ app.js
@@ -11,6 +11,22 @@ app.engine('.hbs', expressHbs({ extname: '.hbs', layoutsDir: 'views' }));
 app.set('view engine', '.hbs');
 app.set('views', 'views');
 
+/* serve Brotli compressed CSS files wheneve there is a request for .css file */
+app.get('*.css', (req, res, next) => {
+  req.url = req.url + '.br';
+  res.set('Content-Encoding', 'br');
+  res.set('Content-Type', 'text/css; charset=utf-8');
+  next();
+});
+
+/* serve Brotli compressed JS files whenever there is a request for .js file */
+app.get('*.js', (req, res, next) => {
+  req.url = req.url + '.br';
+  res.set('Content-Encoding', 'br');
+  res.set('Content-Type', 'application/javascript; charset=UTF-8');
+  next();
+});
+
 // set static folder
 app.use(express.static(path.join(__dirname, 'dist')));
 
--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ const { src, dest, parallel, watch } = require('gulp');
 const cleanCSS = require('gulp-clean-css');
 const uglify = require('gulp-uglify-es').default;
 const rename = require('gulp-rename');
+const gulpBrotli = require('gulp-brotli');
+const zlib = require('zlib');
 
 const paths = {
   css: {
@@ -14,23 +16,48 @@ const paths = {
   },
 };
 
+/*
+ * Task to Minify and Compress CSS
+ * This task takes a file suppose styles.css from assets/css
+ * Minifies it and compresses the file using Brotli Algorithm
+ * And saves the file as styles.min.css.br in dist/css directory
+ */
 function minifyAndCompressCSS() {
   return src(paths.css.src)
     .pipe(cleanCSS())
     .pipe(rename({ extname: '.min.css' }))
+    .pipe(gulpBrotli(brotliOptions()))
+    .pipe(rename({ extname: '.br' }))
     .pipe(dest(paths.css.dest));
 }
 
+/*
+ * Task to Minify and Compress JavaScript
+ * This task takes a file suppose main.js from assets/js
+ * Minifies it and compresses the file using Brotli Algorithm
+ * And saves the file as main.min.js.br in dist/js directory
+ */
 function minifyAndCompressJS() {
   return src(paths.js.src)
     .pipe(uglify())
     .pipe(rename({ extname: '.min.js' }))
+    .pipe(gulpBrotli(brotliOptions()))
+    .pipe(rename({ extname: '.br' }))
     .pipe(dest(paths.js.dest));
 }
 
+// task to watch files for change in dev environment
 function watchFiles() {
   watch(paths.css.src, { ignoreInitial: false }, minifyAndCompressCSS);
   watch(paths.js.src, { ignoreInitial: false }, minifyAndCompressJS);
+}
+
+function brotliOptions() {
+  return {
+    params: {
+      [zlib.constants.BROTLI_PARAM_QUALITY]: zlib.constants.BROTLI_MAX_QUALITY,
+    },
+  };
 }
 
 exports.watch = watchFiles;
